7b07eecc011fa860ea735c379f127aa458efa9c1
Seunghyeon Lee <hyeon970315@gmail.com>
2025-01-05 09:58:05 +0900

    [level 2] Title: 짝지어 제거하기, Time: 69.25 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if (stack.isEmpty() || stack.peek() != c) {
+                stack.push(c);
+            } else {
+                stack.pop();
+            }
+        }
+
+        if(stack.isEmpty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
