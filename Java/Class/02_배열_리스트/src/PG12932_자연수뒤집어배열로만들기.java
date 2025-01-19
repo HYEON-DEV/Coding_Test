d91c1d5e149f01bd91d624545e7f5189d6cdfda9
hyeon <hyeon970315@gmail.com>
2025-01-19 12:45:27 +0900

    배열, 리스트 관련 프로그래머스 문제 풀기 / java.lang 패키지, StringBuilder 공부

--- Java/Class/02_배열_리스트/src/PG12932_자연수뒤집어배열로만들기.java
+++ Java/Class/02_배열_리스트/src/PG12932_자연수뒤집어배열로만들기.java
@@ -4,7 +4,7 @@ public class PG12932_자연수뒤집어배열로만들기 {
     
     public int[] solution(long n) {
 
-        char[] c = String.valueOf(n).toCharArray();      
+        /* char[] c = String.valueOf(n).toCharArray();      
         int size = c.length;
 
         for(int i=0; i<size/2; i++) {
@@ -19,12 +19,21 @@ public class PG12932_자연수뒤집어배열로만들기 {
         for (int i=0; i<size; i++) {
             answer[i] = c[i]-'0';
         }
-        return answer;
+        return answer; */
+
+        String str = new StringBuilder(String.valueOf(n)).reverse().toString();
+        return str.chars().map(c -> c-'0').toArray();
     }
 
     public static void main(String[] args) {
         PG12932_자연수뒤집어배열로만들기 ex = new PG12932_자연수뒤집어배열로만들기();
         System.out.println(Arrays.toString(ex.solution(12345)));
         System.out.println(Arrays.toString(ex.solution(123456)));
+
+        StringBuilder sb = new StringBuilder("abcd");
+        sb.replace(0,2,"eeee");
+        System.out.println(sb);
+        System.out.println(sb.substring(2,4));
+        System.out.println(sb);
     }
 }
