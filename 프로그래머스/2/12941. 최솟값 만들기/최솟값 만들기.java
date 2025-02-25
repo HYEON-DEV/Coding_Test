e8c5eeedced8c83bad278e5733877361cb509ac6
Seunghyeon Lee <hyeon970315@gmail.com>
2025-02-25 18:09:18 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.08 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int sum = 0;
+        int size = A.length;
+
+        for (int i=0; i<size; i++) {
+            sum += A[i] * B[size-i-1];
+        }
+        
+        return sum;
+    }
+}
