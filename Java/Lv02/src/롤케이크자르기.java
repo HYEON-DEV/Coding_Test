85c0deab1f45ce24ac46580d735bb6311eb377e8
hyeon <hyeon970315@gmail.com>
2025-04-24 20:43:48 +0900

    프로그래머스-Java _ 롤케이크자르기

--- Java/Lv02/src/롤케이크자르기.java
+++ Java/Lv02/src/롤케이크자르기.java
@@ -20,17 +20,17 @@ public class 롤케이크자르기 {
             q2.add(i);
         }
         // System.out.println(q2);
-        // System.out.println(q2.poll());
-        // System.out.println(q2);
 
         int result = 0;
-        // int cnt1=0, cnt2=0;
         int cnt1, cnt2;
-        while (true) {
+        while (true) {  // ⏰ 시간복잡도는 O(N²)
             q1.add(q2.poll());
             System.out.printf("q1: %s,  q2: %s \n", q1, q2);
 
             cnt1 = (int) Arrays.stream(q1.toArray(new Integer[0])).distinct().count();
+            // q1.toArray(new Integer[0])는 O(N)
+            // stream().distinct()는 내부적으로 HashSet을 써서 O(N)
+            // Queue 두 개, toArray() 내부 복사 등도 불필요한 메모리를 많이 사용한다 
             cnt2 = (int) Arrays.stream(q2.toArray(new Integer[0])).distinct().count();
             
             if (cnt1 > cnt2) break;
@@ -39,16 +39,6 @@ public class 롤케이크자르기 {
                 result++;
             }
         }
-        // for (int i=0; i<topping.length-1; i++) {
-        //     q1.add(q2.poll());
-        //     System.out.printf("q1: %s,  q2: %s \n", q1, q2);
-
-        //     cnt1 = (int) Arrays.stream(q1.toArray(new Integer[0])).distinct().count();
-        //     cnt2 = (int) Arrays.stream(q2.toArray(new Integer[0])).distinct().count();
-        //     if (cnt1 == cnt2) {
-        //         result++;
-        //     }
-        // }
 
         return result;
     }
@@ -56,7 +46,7 @@ public class 롤케이크자르기 {
     public static void main(String[] args) {
         롤케이크자르기 s = new 롤케이크자르기();
         System.out.println(s.solution(new int[] {1, 2, 1, 3, 1, 4, 1, 2}));
-        // System.out.println();
-        // System.out.println(new int[] {1,2,3,1,4});
+        System.out.println();
+        System.out.println(s.solution(new int[] {1,2,3,1,4}));
     }
 }
