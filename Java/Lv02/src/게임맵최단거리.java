e1226b9220516e1df8de166418dddea3dcc9b685
hyeon <hyeon970315@gmail.com>
2025-04-26 22:22:36 +0900

    프로그래머스-MySQL _ DATETIME에서 DATE로 형 변환

--- Java/Lv02/src/게임맵최단거리.java
+++ Java/Lv02/src/게임맵최단거리.java
@@ -17,25 +17,23 @@ import java.util.Queue;
 public class 게임맵최단거리 {
 
     public int solution(int[][] maps) {
-
         int n = maps.length;
         int m = maps[0].length;
 
         boolean[][] visited = new boolean[n][m];
         visited[0][0] = true;
 
-        // 상하좌우
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
+        int[] dx = {-1,1,0,0};  // 상하
+        int[] dy = {0,0,-1,1};  // 좌우 
 
-        // x좌표, y좌표, 이동횟수 
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {0,0,1});
+        queue.offer(new int[] {0,0,1});     // x좌표, y좌표, 이동횟수 
         
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
             int x=current[0], y=current[1], dist=current[2];
             System.out.printf("x: %d,  y: %d,  d: %d \n", x, y, dist);
+            System.out.printf("queue: %s \n", Arrays.deepToString(queue.toArray()));
 
             // 도착지에 도달한 경우
             if (x==n-1 && y==m-1) {
@@ -51,9 +49,13 @@ public class 게임맵최단거리 {
                 if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]) {
                     visited[nx][ny] = true;
                     queue.offer(new int[] {nx, ny, dist+1});  // 다음 위치 저장 
+                    System.out.printf("maps[%d][%d] = %d,  visited: %b \n", nx, ny, maps[nx][ny], visited[nx][ny]);
+                } else if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 0 && !visited[nx][ny]){
+                    System.out.printf("maps[%d][%d] = %d,  visited: %b \n", nx, ny, maps[nx][ny], visited[nx][ny]);
                 }
             }
-            System.out.println(Arrays.deepToString(queue.toArray()));
+            System.out.printf("queue: %s \n", Arrays.deepToString(queue.toArray()));
+            System.out.println();
         }
 
         return -1;
